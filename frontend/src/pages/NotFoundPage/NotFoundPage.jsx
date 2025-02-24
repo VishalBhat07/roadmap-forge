9421e2ba6fdafe4aeb90e8bef861d4507a0a4bfb
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-02-24 09:01:20 +0530

    Aded fuzzy text animation on notFoundPage

--- frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import "./NotFoundPage.css";
 import { useNavigate } from "react-router-dom";
+import FuzzyText from "../../blocks/FuzzyText/FuzzyText";
 
 const NotFoundPage = () => {
   const navigate = useNavigate();
   return (
     <div className="container">
-      <div className="number">404</div>
-      <div className="text1">There's NOTHING here...</div>
+      <div className="number">
+        <FuzzyText
+          children
+          fontSize={"220px"}
+          fontWeight={900}
+          fontFamily={"inherit"}
+          color={"red"}
+          enableHover={true}
+          baseIntensity={0.25}
+          hoverIntensity={0.5}
+        >
+          404
+        </FuzzyText>
+      </div>
+      <div className="text1">
+        <FuzzyText
+          children
+          fontSize={"40px"}
+          fontWeight={900}
+          fontFamily={"inherit"}
+          color={"red"}
+          enableHover={true}
+          baseIntensity={0.2}
+          hoverIntensity={0.5}
+        >
+          There's NOTHING here...
+        </FuzzyText>
+      </div>
       <div className="text2">
         ...maybe the page you're looking for is not found or never existed
       </div>
