8be438fa09a3b04aa5bb7f745e45f6887a9fba95
VishalBhat07 <vishalbhat21092005@gmail.com>
2025-02-27 11:16:03 +0530

    Changed allRoadmaps structure and added desgin on roadmap page

--- frontend/src/pages/Roadmap/Roadmap.jsx
+++ frontend/src/pages/Roadmap/Roadmap.jsx
@@ -8,7 +8,18 @@ const Roadmap = () => {
   const params = useParams();
   return (
     <div className={styles.container}>
-      <RoadmapGenerator roadmap={allRoadmaps[params.id]} />
+      <div className={styles.header}>
+        <div className={styles.courseInfo}>{allRoadmaps[params.id].title}</div>
+        <div className={styles.buttons}>
+          <button>
+            <p> Start learning</p> <i class="fa-solid fa-graduation-cap"></i>
+          </button>
+          <button>
+            <p> Join Community</p> <i class="fa-brands fa-discord"></i>
+          </button>
+        </div>
+      </div>
+      <RoadmapGenerator roadmap={allRoadmaps[params.id].roadmap} />
     </div>
   );
 };
--- frontend/src/pages/Roadmap/allRoadmaps.js
+++ frontend/src/pages/Roadmap/allRoadmaps.js
@@ -5,25 +5,55 @@ import dsaRoadmap from "../../assets/Roadmaps/dsaRoadmap";
 import mlRoadmap from "../../assets/Roadmaps/mlRoadmap";
 import mobileappdevelopmentreactnativeRoadmap from "../../assets/Roadmaps/mobileappdevelopmentreactnativeRoadmap";
 import cybersecurityRoadmap from "../../assets/Roadmaps/cybersecurityRoadmap";
-import blockchaindevelopmentRoadmap from "../../assets/Roadmaps/blockchaindevelopmentRoadmap";
-import gamedevelopmentRoadmap from "../../assets/Roadmaps/gamedevelopmentRoadmap";
-import devopscloudcomputingRoadmap from "../../assets/Roadmaps/devopscloudcomputingRoadmap";
-import artificialintelligenceRoadmap from "../../assets/Roadmaps/artificialintelligenceRoadmap";
-import cgamedevelopmentRoadmap from "../../assets/Roadmaps/cgamedevelopmentRoadmap";
+import blockchainDevelopmentRoadmap from "../../assets/Roadmaps/blockchainDevelopmentRoadmap";
+import gameDevelopmentRoadmap from "../../assets/Roadmaps/gameDevelopmentRoadmap";
+import devOpsCloudComputingRoadmap from "../../assets/Roadmaps/devOpsCloudComputingRoadmap";
+import artificialIntelligenceRoadmap from "../../assets/Roadmaps/artificialIntelligenceRoadmap";
+import cGameDevelopmentRoadmap from "../../assets/Roadmaps/cGameDevelopmentRoadmap";
 
 const allRoadmaps = {
-  frontendwebdevelopment: frontendRoadmap,
-  backendwebdevelopment: backendRoadmap,
-  fullstackdevelopmentmern: fullStackRoadmap,
-  datastructuresalgorithms: dsaRoadmap,
-  machinelearning: mlRoadmap,
-  mobileappdevelopmentreactnative: mobileappdevelopmentreactnativeRoadmap,
-  cybersecurity: cybersecurityRoadmap,
-  blockchaindevelopment: blockchaindevelopmentRoadmap,
-  devopscloudcomputing: devopscloudcomputingRoadmap,
-  gamedevelopment: gamedevelopmentRoadmap,
-  artificialintelligence: artificialintelligenceRoadmap,
-  cgamedevelopment: cgamedevelopmentRoadmap,
+  frontendwebdevelopment: {
+    roadmap: frontendRoadmap,
+    title: "Frontend Web Development",
+  },
+  backendwebdevelopment: {
+    roadmap: backendRoadmap,
+    title: "Backend Web Development",
+  },
+  fullstackdevelopmentmern: {
+    roadmap: fullStackRoadmap,
+    title: "Full Stack Development (MERN)",
+  },
+  datastructuresalgorithms: {
+    roadmap: dsaRoadmap,
+    title: "Data Structures & Algorithms",
+  },
+  machinelearning: { roadmap: mlRoadmap, title: "Machine Learning" },
+  mobileappdevelopmentreactnativeRoadmap: {
+    roadmap: mobileappdevelopmentreactnativeRoadmap,
+    title: "Mobile App Development (React Native)",
+  },
+  cybersecurity: { roadmap: cybersecurityRoadmap, title: "Cybersecurity" },
+  blockchaindevelopment: {
+    roadmap: blockchainDevelopmentRoadmap,
+    title: "Blockchain Development",
+  },
+  devopscloudcomputing: {
+    roadmap: devOpsCloudComputingRoadmap,
+    title: "DevOps & Cloud Computing",
+  },
+  gamedevelopment: {
+    roadmap: gameDevelopmentRoadmap,
+    title: "Game Development",
+  },
+  artificialintelligence: {
+    roadmap: artificialIntelligenceRoadmap,
+    title: "Artificial Intelligence",
+  },
+  cgamedevelopment: {
+    roadmap: cGameDevelopmentRoadmap,
+    title: "C++ Game Development",
+  },
 };
 
 export default allRoadmaps;
